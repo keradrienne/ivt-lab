0b9ac66a88054d7ae59fa76f0477929b702a1c49
keradrienne <56718907+keradrienne@users.noreply.github.com>
2024-04-26 11:36:28 +0200

    Update TorpedoStore.java

--- src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
+++ src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
@@ -31,6 +31,7 @@ public class TorpedoStore {
 
   public boolean fire(int numberOfTorpedos){
     if(numberOfTorpedos < 1 || numberOfTorpedos > this.torpedoCount){
+      //Exception
       throw new IllegalArgumentException("numberOfTorpedos");
     }
 
