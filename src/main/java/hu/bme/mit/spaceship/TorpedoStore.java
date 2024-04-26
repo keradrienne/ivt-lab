ab4255e2ce32671f13b485221b22d246a157ca7b
keradrienne <ker.adrienne@gmail.com>
2024-04-26 11:24:28 +0200

    Update TorpedoStore.java

--- src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
+++ src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
@@ -8,7 +8,7 @@ import java.util.Random;
 * (Deliberately contains bugs.)
 */
 public class TorpedoStore {
-  private Random random = new Random();  // Compliant
+  private Random generator = new Random();  // Compliant
 
   // rate of failing to fire torpedos [0.0, 1.0]
   private double FAILURE_RATE = 0.2; //NOSONAR
